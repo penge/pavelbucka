b2c478677e1c044998eacb98aafc7750e4ce6891
Pavel Bucka <pavelbucka@icloud.com>
2014-08-22 14:07:19 +0200

    Define package.json, gulpfile.js, config.rb

---
+++ config.rb
@@ -0,0 +1,2 @@
+sass = 'app/styles'
+css  = 'dist/styles'
---
+++ gulpfile.js
@@ -0,0 +1,61 @@
+'use strict';
+
+var //var
+  browserSync    = require('browser-sync'),
+  browserify     = require('gulp-browserify'),
+  cleanhtml      = require('gulp-cleanhtml'),
+  compass        = require('gulp-compass'),
+  gulp           = require('gulp'),
+  rename         = require('gulp-rename'),
+  runSequence    = require('run-sequence'),
+  uglify         = require('gulp-uglify');
+
+gulp.task('browser-sync', function() {
+  browserSync({
+    notify: false,
+    server: {
+      baseDir: 'dist'
+    }
+  });
+});
+
+gulp.task('reload', function () {
+  browserSync.reload();
+});
+
+gulp.task('htmls', function () {
+  return gulp.src('./app/*.html')
+    .pipe(cleanhtml())
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('styles', function () {
+  return gulp.src('./app/styles/**/*.sass')
+    .pipe(compass({
+      style: 'compressed',
+      sass: 'app/styles',
+      css: 'dist/styles' 
+    }))
+    .pipe(gulp.dest('./dist/styles'));
+});
+
+gulp.task('scripts', function() {
+  return gulp.src('./app/scripts/main.js')
+    .pipe(browserify())
+    .pipe(uglify())
+    .pipe(rename('bundle.js'))
+    .pipe(gulp.dest('./dist/scripts'));
+});
+
+gulp.task('default', ['browser-sync'], function () {
+  runSequence(['htmls', 'styles', 'scripts']);
+
+  gulp.watch('./app/*.html',
+    ['htmls', 'reload']);
+ 
+  gulp.watch('./app/styles/**/*.sass',
+    ['styles', 'reload']);
+
+  gulp.watch('./app/scripts/**/*.js',
+    ['scripts', 'reload']);
+});
