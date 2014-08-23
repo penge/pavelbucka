65db5c00020c8dce1a3f49d8b27de248812225f5
Pavel Bucka <pavelbucka@icloud.com>
2014-08-23 21:07:58 +0200

    Define jshint task

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var //var
   cleanhtml      = require('gulp-cleanhtml'),
   compass        = require('gulp-compass'),
   gulp           = require('gulp'),
+  jshint         = require('gulp-jshint'),
   rename         = require('gulp-rename'),
   runSequence    = require('run-sequence'),
   uglify         = require('gulp-uglify');
@@ -39,7 +40,13 @@ gulp.task('styles', function () {
     .pipe(gulp.dest('./dist/styles'));
 });
 
-gulp.task('scripts', function() {
+gulp.task('jshint', function() {
+  return gulp.src('./app/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('scripts', ['jshint'], function() {
   return gulp.src('./app/scripts/main.js')
     .pipe(browserify())
     .pipe(uglify())
