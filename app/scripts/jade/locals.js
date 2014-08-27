e5b38628094af3cff20f0842b9b8f1a709f66ada
Pavel Bucka <pavelbucka@icloud.com>
2014-08-27 17:10:27 +0200

    Define locals.js

---
+++ app/scripts/jade/locals.js
@@ -0,0 +1,79 @@
+var locals = {
+  author: "Pavel Bucka",
+  description: "I'm Pavel Bucka and I do professional web development using Ruby on Rails mainly.",
+  email: "pavelbucka@icloud.com",
+  helps: [
+    "Build & deliver great web application",
+    "Teach & learn new web technologies",
+    "Promote & sell your product",
+  ],
+  reasons: [
+    "I love to keep things simple",
+    "I have a vision",
+    "I have cool references",
+  ],
+  projects: [
+    {
+      name: "TakeScreen",
+      desc: "user friendly and effective screen capturing tool",
+      url: "http://www.stahuj.centrum.cz/grafika_a_design/zachycovani_obrazovky/takescreen",
+    },
+    {
+      name: "FolderScan",
+      desc: "personal data catalogues done right",
+      url: "http://www.stahuj.centrum.cz/utility_a_ostatni/prace_se_soubory/souborove_manazery/folderscan",
+    },
+    {
+      name: "PixelTest",
+      desc: "monitors' pixels functionality tester",
+      url: "http://www.stahuj.centrum.cz/utility_a_ostatni/benchmarky_a_testy/pixeltest",
+    },
+    {
+      name: "Rename Assistant",
+      desc: "files renaming done quickly",
+      url: "http://www.stahuj.centrum.cz/utility_a_ostatni/prace_se_soubory/prevody_souboru/rename-assistant",
+    },
+    {
+      name: "MonitorOFF",
+      desc: "monitors' energy saver",
+      url: "http://www.stahuj.centrum.cz/utility_a_ostatni/zjednoduseni_prace/ostatni/monitoroff",
+    },
+  ],
+  works: [
+    {
+      name: "Datamolino",
+      desc: "worked as BE/FE developer, used ROR, Redis, Elasticsearch, AWS",
+      url: "http://www.datamolino.com",
+    },
+    {
+      name: "Aston ITM",
+      desc: "worked as BE/FE developer of Flaps, used GWT, Vaadin",
+      url: "http://www.aston.sk",
+    },
+    {
+      name: "TrashOut",
+      desc: "worked as BE developer, used GAE and Datastore",
+      url: "http://www.trashout.me",
+    },
+    {
+      name: "KROS",
+      desc: "created great tendering app in ASP.NET MVC with Telerik Extensions",
+      url: "http://www.kros.sk",
+    },
+    {
+      name: "SIEMENS",
+      desc: "worked as tester and Java developer",
+      url: "http://www.siemens.sk",
+    },
+  ],
+};
+
+locals.links = {
+  main:       "styles/main.css",
+  bundle:     "scripts/bundle.js",
+  robotoSlab: "http://fonts.googleapis.com/css?family=Roboto+Slab:100",
+  jquery:     "http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js",
+  tweenMax:   "http://cdnjs.cloudflare.com/ajax/libs/gsap/latest/TweenMax.min.js",
+};
+
+module.exports = locals;
