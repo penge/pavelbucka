8d131e5d546a193baec869dc055b6a9c8e0ce627
Pavel Bucka <pavelbucka@icloud.com>
2014-10-14 00:35:56 +0200

    Remove Animator and Challenger usage

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,7 @@
 'use strict';
 
 var Links = require('./links.js');
-var Animator = require('./animator.js'); 
-var Challenger = require('./challenger');
 
 $(function() {
   Links.register();
-  Animator.animate();
-  Challenger.challenge();
 });
