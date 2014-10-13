9e9e1bbed2f5520c82a85dbfe9e9365fe18e0fa2
Pavel Bucka <pavelbucka@icloud.com>
2014-10-14 00:34:49 +0200

    Remove #controls and Controls usage

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,15 +1,10 @@
 'use strict';
 
-var Controls = require('./controls.js');
 var Links = require('./links.js');
 var Animator = require('./animator.js'); 
 var Challenger = require('./challenger');
 
 $(function() {
-  Controls.register(
-    $('#rewind'),
-    $('#forward')
-  );
   Links.register();
   Animator.animate();
   Challenger.challenge();
